823546a986600cb7f0a868f47b32c0d24d58e54c
Víctor Ávila <victoraavila2000@gmail.com>
2024-02-12 14:27:01 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Víctor Ávila on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
