3f82d1b5088867a5d2951ee90ed79c05ae068c3f
Víctor Ávila <victoraavila2000@gmail.com>
2024-02-12 15:40:34 -0300

    Moving ScrollView to a separate View - second working solution

--- Moonshot/CrewView.swift
+++ Moonshot/CrewView.swift
@@ -9,16 +9,13 @@ import SwiftUI
 
 struct CrewView: View {
     // The content from this struct will be shown in this View
-//    struct CrewMember {
-//        let role: String
-//        let astronaut: Astronaut
-//    }
-    
-    // Initially, it will only have a property to track whether the View is being viewed or not.
-//    let mission: Mission
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
     
     // An array of fully resolved CrewMembers, i.e., role + Astronaut already merged from both JSONs
-    let crew: [MissionView.CrewMember]
+    let crew: [CrewMember]
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) { // To not show the scrolling bar below the astronauts
@@ -56,19 +53,27 @@ struct CrewView: View {
     }
     
     // A custom initializer that accepts the Mission it represents, along with all the Astronauts, and then figure out the crew Array.
-    init(crew: [MissionView.CrewMember]) {
-        self.crew = crew
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        // We could loop over the mission crew, and for each one look for its ID in the [str: Astronaut]. When we find one, we would convert it into one of the CrewMembers. If we don't find it, we've got a CrewMember with an invalid name (the latter should never happen, because it's an error in the JSONs. So, this possibility should throw a fatalError()).
+        self.crew = mission.crew.map { member in
+            // Look up the member's name in the [String: Astronaut]
+            if let astronaut = astronauts[member.name] {
+                // Great! We now have the crew member and the astronaut itself (name and description).
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
     }
 }
 
 #Preview {
     // Hopefully, our Bundle Extensions works here too
-//    let missions: [Mission] = Bundle.main.decode("missions.json")
-//    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-//    
-//    return CrewView(crew: crew) // We need to pass in a mission and an astronaut
-//    // This is just for the Preview. It is not changing the code.
-//    // We don't need to set this in our Main View, because the NavigationStack already does so. The whole stack will be set automatically from there.
-//        .preferredColorScheme(.dark)
-    return Text("In development")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return CrewView(mission: missions[4], astronauts: astronauts) // We need to pass in a mission and an astronaut
+    // This is just for the Preview. It is not changing the code.
+    // We don't need to set this in our Main View, because the NavigationStack already does so. The whole stack will be set automatically from there.
+        .preferredColorScheme(.dark)
 }
--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -11,17 +11,10 @@ import SwiftUI
 // We will have to match crew IDs with crew details from both JSONs.
 
 struct MissionView: View {
-    // The content from this struct will be shown in this View
-    struct CrewMember {
-        let role: String
-        let astronaut: Astronaut
-    }
-    
     // Initially, it will only have a property to track whether the View is being viewed or not.
     let mission: Mission
     
-    // An array of fully resolved CrewMembers, i.e., role + Astronaut already merged from both JSONs
-    let crew: [CrewMember]
+    let astronauts: [String: Astronaut]
     
     var body: some View {
         // We will use a Container Relative Frame for the mission badge to set its width correctly
@@ -74,7 +67,7 @@ struct MissionView: View {
                 .padding(.horizontal) // So it stays away from the edges of the screen
                 
                 // The following ScrollView is not inside the above VStack to guarantee that it slides completely from edge to edge (the above VStack has horizontal padding)
-                CrewView(crew: crew)
+                CrewView(mission: mission, astronauts: astronauts)
             }
             .padding(.bottom) // So it doesn't sit right next to the very edge of the bottom of the screen
         }
@@ -86,17 +79,7 @@ struct MissionView: View {
     // A custom initializer that accepts the Mission it represents, along with all the Astronauts, and then figure out the crew Array.
     init(mission: Mission, astronauts: [String: Astronaut]) {
         self.mission = mission
-        
-        // We could loop over the mission crew, and for each one look for its ID in the [str: Astronaut]. When we find one, we would convert it into one of the CrewMembers. If we don't find it, we've got a CrewMember with an invalid name (the latter should never happen, because it's an error in the JSONs. So, this possibility should throw a fatalError()).
-        self.crew = mission.crew.map { member in
-            // Look up the member's name in the [String: Astronaut]
-            if let astronaut = astronauts[member.name] {
-                // Great! We now have the crew member and the astronaut itself (name and description).
-                return CrewMember(role: member.role, astronaut: astronaut)
-            } else {
-                fatalError("Missing \(member.name)")
-            }
-        }
+        self.astronauts = astronauts
     }
 }
 
@@ -105,7 +88,7 @@ struct MissionView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     
-    return MissionView(mission: missions[1], astronauts: astronauts) // We need to pass in a mission and an astronaut
+    return MissionView(mission: missions[2], astronauts: astronauts) // We need to pass in a mission and an astronaut
     // This is just for the Preview. It is not changing the code.
     // We don't need to set this in our Main View, because the NavigationStack already does so. The whole stack will be set automatically from there.
         .preferredColorScheme(.dark)
